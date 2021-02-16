80e2b724bbcd99d0fb08d5b5aac5bfd2039697d0
Tanushree Tumane <tanushreetumane@gmail.com>
2021-02-16 22:18:53 +0530

    Update SpringRedisExampleApplication.java

--- src/main/java/com/redis/cache/springredisexample/SpringRedisExampleApplication.java
+++ src/main/java/com/redis/cache/springredisexample/SpringRedisExampleApplication.java
@@ -4,7 +4,7 @@ import com.redis.cache.springredisexample.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
